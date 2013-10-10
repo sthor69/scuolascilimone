de23a5144c6eb9ee9dc0c240b94d47c105a36309
Sergio Torassa <sertorassa@hotmail.com>
2013-10-10 18:02:21 +0200

    end of day

--- src/com/storassa/android/scuolasci/LoginFragment.java
+++ src/com/storassa/android/scuolasci/LoginFragment.java
@@ -11,11 +11,10 @@ import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.http.client.HttpClient;
-
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,218 +24,249 @@ import android.widget.TextView;
 
 public class LoginFragment extends Fragment {
 
-	String responseString = "";
-	int responseCode;
-	int counter = 0;
-	String username, password;
+   String responseString = "";
+   int responseCode;
+   int counter = 0;
+   String username, password;
 
-	String urlParameters;
-	HttpURLConnection connection;
+   String urlParameters;
+   HttpURLConnection connection;
 
-	// get storage info
-	BasicStorageHelper storageHelper;
+   // get storage info
+   SharedPreferences settings;
+   
+   // login info already get
+   boolean loginInfoAvailable = false;
 
-	// views and activities
-	TextView usernameView;
-	TextView passwordView;
-	Button loginBtn;
-	MainActivity parentActivity;
-	View result;
+   // views and activities
+   TextView usernameView;
+   TextView passwordView;
+   Button loginBtn;
+   MainActivity parentActivity;
+   View result;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		result = inflater.inflate(R.layout.login_fragment, container, false);
+   @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container,
+         Bundle savedInstanceState) {
+      // Inflate the layout for this fragment
+      result = inflater.inflate(R.layout.login_fragment, container, false);
 
-		// get the storage helper
-		storageHelper = BasicStorageHelper.getStorageHelper();
+      settings = getActivity().getPreferences(0);
 
-		// get the usarname and password views
-		usernameView = (TextView) result.findViewById(R.id.username_text);
-		passwordView = (TextView) result.findViewById(R.id.password_text);
+      // get the usarname and password views
+      usernameView = (TextView) result.findViewById(R.id.username_text);
+      passwordView = (TextView) result.findViewById(R.id.password_text);
 
-		// get the parent activity that fired this fragment
-		parentActivity = (MainActivity) getActivity();
+      // get the parent activity that fired this fragment
+      parentActivity = (MainActivity) getActivity();
 
-		// get the login button view and set its OnClickListener
-		loginBtn = (Button) result.findViewById(R.id.login_button);
-		loginBtn.setOnClickListener(new View.OnClickListener() {
+      // get the login button view and set its OnClickListener
+      loginBtn = (Button) result.findViewById(R.id.login_button);
+      loginBtn.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
+         @Override
+         public void onClick(View v) {
 
-				// set the URL params initial string
-				urlParameters = "task=signin&accion=signin";
+            // set the URL params initial string
+            urlParameters = "task=signin&accion=signin";
 
-				// set the username
-				username = getUsername();
-				urlParameters += username;
-				urlParameters += "&textBoxUsername=";
+            // set the username
+            username = getUsername();
+            urlParameters += username;
+            urlParameters += "&textBoxUsername=";
 
-				// set the password
-				password = getPassword();
-				urlParameters += "&textBoxPassword=";
-				urlParameters += password;
+            // set the password
+            password = getPassword();
+            urlParameters += "&textBoxPassword=";
+            urlParameters += password;
 
-				// launch the thread to POST the server
-				ExecutorService exec = Executors.newCachedThreadPool();
-				exec.execute(new Runnable() {
+            // launch the thread to POST the server
+            ExecutorService exec = Executors.newCachedThreadPool();
+            exec.execute(new Runnable() {
 
-					@Override
-					public void run() {
-						sendLoginPost();
+               @Override
+               public void run() {
+                  sendLoginPost();
 
-					}
-				});
+               }
+            });
 
-				// Launch the timer that each REPETITION_TIME check the
-				// response
-				// code
-				Timer timer = new Timer();
-				timer.schedule(new TimerTask() {
+            // Launch the timer that each REPETITION_TIME check the
+            // response
+            // code
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
 
-					@Override
-					public void run() {
+               @Override
+               public void run() {
+                  
+                  checkCode(responseCode);
+                  if (loginInfoAvailable)
+                     this.cancel();
 
-						checkCode(responseCode);
-						this.cancel();
+               } // run in TimerTask (new Runnable) definition
 
-					} // run in TimerTask (new Runnable) definition
+            }, 0, REPETITION_TIME); // TimerTask
 
-				}, 0, REPETITION_TIME); // TimerTask
+         } // onClick in setOnClickListener(new View.OnClickListener())
+           // definition
 
-			} // onClick in setOnClickListener(new View.OnClickListener())
-				// definition
+      }); // setOnClickListener definition
 
-		}); // setOnClickListener definition
+      return result;
+   }
+   
+   public static void main (String[] args) {
+      
+   }
 
-		return result;
-	}
+   private String getUsername() {
+      String result = "";
 
-	private String getUsername() {
-		String result = "";
+      if (settings.getBoolean("remember_me", true)) {
+         result = settings.getString("username", "");
+      } else {
+         result = String.valueOf(usernameView.getText());
+         SharedPreferences.Editor editor = settings.edit();
+         editor.putBoolean("remember_me", true).putString("username", result)
+               .commit();
+      }
 
-		if (storageHelper.isRememberMe()) {
-			result = storageHelper.getUsername();
-		} else {
-			result = String.valueOf(usernameView.getText());
-		}
+      return result;
+   }
 
-		return result;
-	}
+   private String getPassword() {
+      String result = "";
 
-	private String getPassword() {
-		String result = "";
+      if (settings.getBoolean("remember_me", true)) {
+         result = settings.getString("password", "");
+      } else {
+         result = String.valueOf(passwordView.getText());
+         SharedPreferences.Editor editor = settings.edit();
+         editor.putBoolean("remember_me", true).putString("password", result)
+               .commit();
+      }
 
-		if (storageHelper.isRememberMe()) {
-			result = storageHelper.getPassword();
-		} else {
-			result = String.valueOf(passwordView.getText());
-		}
+      return result;
+   }
 
-		return result;
-	}
+   private void checkCode(int code) {
+      // if the response code is 303, the password is correct
+      if (code == 303) {
+         String[] availableButtons = getAvailBtn(responseString);
+         
+         // set availability to true to cancel the timer
+         loginInfoAvailable = true;
+         
+         // wait WAITING_TICKS * REPETITION_TIME mseconds to receive
+         // 303
+      } else if (counter < WAITING_TICKS) {
+         counter++;
 
-	private void checkCode(int code) {
-		// if the response code is 303, the password is correct
-		if (responseCode == 303) {
-			HttpConnectionHelper httpHelper = HttpConnectionHelper.getHelper();
-			String location = connection.getHeaderField("Location");
-			
-			try {
-				httpHelper.openGenericConnection(location);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+         // if the response code is 200, the password is incorrect
+      } else if (code == 200) {
+         parentActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+               AlertDialog.Builder builder = new AlertDialog.Builder(
+                     parentActivity);
 
-			// wait WAITING_TICKS * REPETITION_TIME mseconds to receive
-			// 303
-		} else if (counter < WAITING_TICKS) {
-			counter++;
+               builder.setMessage("Wrong password").setTitle(
+                     String.valueOf("INFO"));
+               builder.setPositiveButton(R.string.ok,
+                     new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                           parentActivity.finish();
+                        } // onClick
+                     }); // OnClickListener
 
-			// if the response code is 200, the password is incorrect
-		} else if (responseCode == 200) {
-			parentActivity.runOnUiThread(new Runnable() {
+               AlertDialog dialog = builder.create();
+               dialog.show();
+               
+               // set availability to true to cancel the timer
+               loginInfoAvailable = true;
 
-				@Override
-				public void run() {
-					AlertDialog.Builder builder = new AlertDialog.Builder(
-							parentActivity);
+            } // run
+         }); // runOnUiThread
 
-					builder.setMessage("Wrong password").setTitle(
-							String.valueOf("INFO"));
-					builder.setPositiveButton(R.string.ok,
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									parentActivity.finish();
-								} // onClick
-							}); // OnClickListener
+      } else {
+         parentActivity.runOnUiThread(new Runnable() {
 
-					AlertDialog dialog = builder.create();
-					dialog.show();
-				} // run
-			}); // runOnUiThread
+            @Override
+            public void run() {
+               CommonHelper.exitMessage(R.string.http_issue,
+                     R.string.http_issue_dialog_title, parentActivity);
+            } // run
+         }); // Runnable
+      } // else
 
-		} else {
-			parentActivity.runOnUiThread(new Runnable() {
+   }
 
-				@Override
-				public void run() {
-					CommonHelper.exitMessage(R.string.http_issue,
-							R.string.http_issue_dialog_title, parentActivity);
-				} // run
-			}); // Runnable
-		} // else
+   private void sendLoginPost() {
 
-	}
+      // set the URL
+      final String params = urlParameters;
+      final URL url;
 
-	private void sendLoginPost() {
+      try {
+         url = new URL(SIGNING_URL);
+      } catch (MalformedURLException e1) {
+         e1.printStackTrace();
+         throw new RuntimeException(e1);
+      }
 
-		// set the URL
-		final String params = urlParameters;
-		final URL url;
+      try {
+         connection = (HttpURLConnection) url.openConnection();
+         connection.setDoOutput(true);
+         connection.setDoInput(true);
+         connection.setInstanceFollowRedirects(false);
+         connection.setRequestMethod("POST");
+         connection.setRequestProperty("Content-Type",
+               "application/x-www-form-urlencoded");
+         connection.setRequestProperty("charset", "utf-8");
+         connection.setRequestProperty("Content-Length", ""
+               + Integer.toString(params.getBytes().length));
+         connection.setUseCaches(false);
 
-		try {
-			url = new URL(SIGNING_URL);
-		} catch (MalformedURLException e1) {
-			e1.printStackTrace();
-			throw new RuntimeException(e1);
-		}
+         DataOutputStream wr = new DataOutputStream(connection
+               .getOutputStream());
+         wr.writeBytes(params);
+         wr.flush();
+         wr.close();
+         BufferedInputStream in = new BufferedInputStream(connection
+               .getInputStream());
+         responseCode = connection.getResponseCode();
 
-		try {
-			connection = (HttpURLConnection) url.openConnection();
-			connection.setDoOutput(true);
-			connection.setDoInput(true);
-			connection.setInstanceFollowRedirects(false);
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("Content-Type",
-					"application/x-www-form-urlencoded");
-			connection.setRequestProperty("charset", "utf-8");
-			connection.setRequestProperty("Content-Length",
-					"" + Integer.toString(params.getBytes().length));
-			connection.setUseCaches(false);
+         java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
+         responseString = s.hasNext() ? s.next() : "";
+      } catch (IOException e) {
+         e.printStackTrace();
+         throw new RuntimeException(e);
+      }
+   }
 
-			DataOutputStream wr = new DataOutputStream(
-					connection.getOutputStream());
-			wr.writeBytes(params);
-			wr.flush();
-			wr.close();
-			BufferedInputStream in = new BufferedInputStream(
-					connection.getInputStream());
-			responseCode = connection.getResponseCode();
+   private String[] getAvailBtn(String response) {
+      String temp = response;
+      String[] result = null;
+      int index = 0, start, end;
+      boolean endOfString = false;
 
-			java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
-			responseString = s.hasNext() ? s.next() : "";
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
+      while (!endOfString) {
+         start = temp.indexOf("txt18\">");
+         temp = temp.substring(start + 1);
 
-	private final static int REPETITION_TIME = 1000;
-	private final static int WAITING_TICKS = 10;
-	private final static String SIGNING_URL = "http://www.scuolascilimone.com/it/area-riservata/access/signin";
-	// private final static String SIGNING_URL = "155.132.54.41";
+         end = temp.indexOf("<");
+         result[index++] = temp.substring(start, end);
+
+         if (temp.indexOf("</p>") < temp.indexOf("txt18"))
+            endOfString = true;
+      }
+
+      return result;
+   }
+
+   private final static int REPETITION_TIME = 1000;
+   private final static int WAITING_TICKS = 10;
+   private final static String SIGNING_URL = "http://www.scuolascilimone.com/it/area-riservata/access/signin";
+   // private final static String SIGNING_URL = "155.132.54.41";
 }
