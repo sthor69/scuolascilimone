30c93cd6594db986befcb8f5dbbc41f14007324c
sthor69 <sertorassa@hotmail.com>
2013-11-26 23:33:15 +0100

    Fixes #40. Fixes #41. Added comments and contacts activities.

--- src/com/storassa/android/scuolasci/FeedbackActivity.java
+++ src/com/storassa/android/scuolasci/FeedbackActivity.java
@@ -1,15 +1,66 @@
 package com.storassa.android.scuolasci;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FeedbackActivity extends Activity {
+
+	Button okBtn, cancelBtn;
+	EditText text;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_feedback);
+
+		text = (EditText)findViewById(R.id.feedback_txt);
+		
+		okBtn = (Button) findViewById(R.id.feedback_ok_btn);
+		okBtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// it's not ACTION_SEND
+				Intent intent = new Intent(Intent.ACTION_SENDTO); 
+				intent.setType("text/plain");
+				intent.putExtra(Intent.EXTRA_SUBJECT,
+						"Feedback");
+				// TODO put the correct string in the booking email
+				String body = getEmailBody();
+				intent.putExtra(Intent.EXTRA_TEXT, body);
+				intent.setData(Uri.parse("mailto:sertorassa@hotmail.com")); 
+				// this will make such that when user returns to your app,
+				// your app is displayed, instead of the email app.
+				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
+				startActivity(intent);
+				finish();
+
+			}
+		});
+
+		cancelBtn = (Button) findViewById(R.id.feedback_cancel_btn);
+		cancelBtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				finish();
+			}
+		});
+	}
+	
+	private String getEmailBody() {
+		String result = "";
+		
+		result += "Commento ricevuto da: " + getIntent().getStringExtra("customer");
+		result += "\n\n" + text.getText();
+		
+		return result;
 	}
 
 	@Override
--- src/com/storassa/android/scuolasci/MainActivity.java
+++ src/com/storassa/android/scuolasci/MainActivity.java
@@ -175,6 +175,7 @@ public class MainActivity extends Activity implements HttpResultCallable {
    
    private void showFeedback() {
 	   Intent intent = new Intent(this, FeedbackActivity.class);
+	   intent.putExtra("customer", customerName);
 	   startActivity(intent);
    }
 
